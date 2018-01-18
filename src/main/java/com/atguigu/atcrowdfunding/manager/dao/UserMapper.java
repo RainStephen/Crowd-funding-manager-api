2ae77c64c0c857cf874ad6f3a745c1742023c568
zhuima522 <jieyu_job@sina.com>
2018-01-19 00:04:54 +0800

    实现了后台的权限管理中的用户维护中的条件分页查询

--- src/main/java/com/atguigu/atcrowdfunding/manager/dao/UserMapper.java
+++ src/main/java/com/atguigu/atcrowdfunding/manager/dao/UserMapper.java
@@ -1,9 +1,12 @@
 package com.atguigu.atcrowdfunding.manager.dao;
-
 import com.atguigu.atcrowdfunding.bean.User;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+
+@Component
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -17,4 +20,14 @@ public interface UserMapper {
     
     //根据请求登录的参数从数据库中获取用户数据
 	User queryUserlogin(Map<String, Object> paramMap);
+
+	List<User> queryList(@Param("startIndex") int startIndex, @Param("pageSize") int pageSize);
+	List<User> queryList(@Param("startIndex") int startIndex, @Param("pageSize") int pageSize,@Param("where") String where);
+
+	@Deprecated
+	int queryCount();
+
+    List<User> queryList(Map<String, Object> paramMap);
+
+    int queryCount(Map<String, Object> paramMap);
 }
--- src/main/java/com/atguigu/atcrowdfunding/manager/service/UserService.java
+++ src/main/java/com/atguigu/atcrowdfunding/manager/service/UserService.java
@@ -1,11 +1,18 @@
 package com.atguigu.atcrowdfunding.manager.service;
-
 import java.util.Map;
-
 import com.atguigu.atcrowdfunding.bean.User;
+import com.atguigu.atcrowdfunding.util.Page;
+import org.springframework.stereotype.Service;
 
+@Service("userService")
 public interface UserService {
 
 	User queryUserlogin(Map<String, Object> paramMap);
 
+	@Deprecated
+	Page<User> queryPage(int pageNo, int pageSize);
+
+	public Page<User> queryPage(int pageNo, int pageSize,Map<String, Object> map);
+
+	int save(User user);
 }
